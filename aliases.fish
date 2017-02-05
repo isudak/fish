7b149f852ced0432d3fe74e0cd9050ef348ed5e0
s4 <s4inbox@gmail.com>
2017-02-05 23:11:58 +0300

    WIP: setup LS_COLORS/LSCOLORS

--- aliases.fish
+++ aliases.fish
@@ -1,29 +1,53 @@
-switch (uname -s)
-    case Linux
-        if test -r ~/.dir_colors
-            eval (dircolors -c ~/.dir_colors | string replace -r '^setenv ' 'set -gx ')
-        end
+function  __fish_dircolors
+    command dircolors $argv
+end
 
-        function ls --description 'List contents of directory'
-            if isatty 1
-                command ls \
-                --color=always \
-                --indicator-style=classify \
-                --group-directories-first \
-                --time-style=long-iso \
-                -vxh $argv \
-                | less -XR --quit-if-one-screen
-            else
-                command ls $argv
+function __fish_ls
+    command ls $argv
+end
+
+function __load_dir_colors
+    if test -r ~/.dir_colors
+        eval (__fish_dircolors -c ~/.dir_colors | string replace -r '^setenv ' 'set -gx ')
+    end
+end
+
+function ls --description 'List contents of directory'
+    if isatty 1
+        __fish_ls \
+        --color=always \
+        --indicator-style=classify \
+        --group-directories-first \
+        --time-style=long-iso \
+        -vxh $argv \
+        | less -XR --quit-if-one-screen
+    else
+        __fish_ls $argv
+    end
+end
+
+switch (uname -s)
+    case Darwin FreeBSD
+        if command -s gdircolors > /dev/null; and command -s gls > /dev/null
+            function  __fish_dircolors
+                command gdircolors $argv
+            end
+
+            function __fish_ls
+                command gls $argv
+            end
+        else
+            function __load_dir_colors
+                set -gx LSCOLORS (bsd_dircolors ~/.dir_colors ^ /dev/null)
+            end
+            function ls --description 'List contents of directory'
+                command ls -GFh $argv
             end
         end
-
-    case Darwin FreeBSD
-        eval (bsd_dircolors ~/.dir_colors)
-        function ls --description 'List contents of directory'
-            command ls -GFh $argv
-        end
 end
+
+__load_dir_colors
+functions -e __load_dir_colors
 
 
 function la --description "List contents of directory, including hidden files"
